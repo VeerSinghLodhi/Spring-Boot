d06f9cf5c811d04ccb36da7083bc5a08f71bb080
Veer Singh Lodhi <veerlodhi54@gmail.com>
2025-04-02 11:18:28 +0530

    Create FirstProgram.java

---
+++ FirstProgram.java
@@ -0,0 +1,24 @@
+/*
+
+    -------------ANNOTATIONS---------------------
+
+    1. @SpringBootApplication
+    This annotation is used at the entry point of a Spring Boot application.
+    
+    Example:
+   ------------------------------------------------------------------------------
+   |  import org.springframework.boot.SpringApplication;                        |
+   |  import org.springframework.boot.autoconfigure.SpringBootApplication;      |
+   |  @SpringBootApplication                                                    |
+   |  public class MySpringBootApplication {                                    |
+   |    public static void main(String[] args) {                                |
+   |        SpringApplication.run(MySpringBootApplication.class, args);         |
+   |    }                                                                       |
+   |  }                                                                         |
+   |                                                                            |
+   ------------------------------------------------------------------------------
+
+
+        
+
+*/
