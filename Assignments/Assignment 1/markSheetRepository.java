3797e19c9e4a96193553ea1db750b2b137769ce8
Veer Singh Lodhi <veerlodhi54@gmail.com>
2025-04-15 00:17:12 +0530

    Add files via upload

---
+++ Assignments/Assignment 1/markSheetRepository.java
@@ -0,0 +1,6 @@
+package com.database2.database2.Assignment;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface markSheetRepository extends CrudRepository<Marksheet,Integer> {
+}
