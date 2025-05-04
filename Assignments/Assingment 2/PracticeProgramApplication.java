de4c2cba443c21bb40513f4c1548f4134a95b567
Veer Singh Lodhi <veerlodhi54@gmail.com>
2025-05-04 22:28:02 +0530

    Add files via upload

---
+++ Assignments/Assingment 2/PracticeProgramApplication.java
@@ -0,0 +1,13 @@
+package com.practice.practiceProgram;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracticeProgramApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PracticeProgramApplication.class, args);
+	}
+
+}
