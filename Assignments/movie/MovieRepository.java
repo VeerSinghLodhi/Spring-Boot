07d025dd86ea32ddc8d6cff27f92947c4aba48d5
Veer Singh Lodhi <veerlodhi54@gmail.com>
2025-06-29 19:12:03 +0530

    Add files via upload

---
+++ Assignments/movie/MovieRepository.java
@@ -0,0 +1,6 @@
+package com.second.secondbatchVeer.movie;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface MovieRepository extends CrudRepository<Movies,Integer> {
+}
