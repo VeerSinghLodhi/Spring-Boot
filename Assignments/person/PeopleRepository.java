5e285866696e13242afc877ffa400766d6667925
Veer Singh Lodhi <veerlodhi54@gmail.com>
2025-06-29 19:13:26 +0530

    Add files via upload

---
+++ Assignments/person/PeopleRepository.java
@@ -0,0 +1,6 @@
+package com.second.secondbatchVeer.person;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface PeopleRepository extends CrudRepository<People,Integer> {
+}
