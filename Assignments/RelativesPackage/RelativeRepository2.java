03f1e0afecea3b833b0c39919e2fc583447890c8
Veer Singh Lodhi <veerlodhi54@gmail.com>
2025-06-30 20:20:39 +0530

    Add files via upload

---
+++ Assignments/RelativesPackage/RelativeRepository2.java
@@ -0,0 +1,9 @@
+package com.second.secondbatchVeer.relative;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.List;
+
+public interface RelativeRepository2 extends JpaRepository<Relatives,Integer> {
+}
