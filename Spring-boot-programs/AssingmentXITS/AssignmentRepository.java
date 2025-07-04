cafeacc8cf6fed0c2894b3365c8a02da9674cef6
Veer Singh Lodhi <veerlodhi54@gmail.com>
2025-07-04 21:59:40 +0530

    Add files via upload

---
+++ Spring-boot-programs/AssingmentXITS/AssignmentRepository.java
@@ -0,0 +1,6 @@
+package com.practiceprograms.practiceprogram2.AssingmentXITS;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AssignmentRepository extends JpaRepository<Assignment,Integer> {
+}
