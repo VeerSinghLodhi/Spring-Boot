46f6c0c2d2ffe2c682f0c70581e4a6a4a5d3124b
Veer Singh Lodhi <veerlodhi54@gmail.com>
2025-09-25 22:59:28 +0530

    Add files via upload

---
+++ Spring-boot-programs/Image/ImagesRepository.java
@@ -0,0 +1,7 @@
+package com.xansa.DBconnect.dbconnect.Image;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ImagesRepository extends JpaRepository<Images , Integer> {
+
+}
